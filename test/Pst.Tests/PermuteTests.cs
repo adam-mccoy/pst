462de553a3c4f664715cd8213b5ec7dd68fa7efc
Adam McCoy <adam@forsedomani.com>
2016-01-05 18:06:06 +1100

    Add permute encoder/decoder.

---
+++ test/Pst.Tests/PermuteTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Pst.Internal;
+
+namespace Pst.Tests
+{
+    [TestFixture]
+    public class PermuteTests
+    {
+        [Test]
+        public void Permute_Encrypts()
+        {
+            var input = new byte[]
+            {
+                0x01, 0x02, 0x03, 0x04, 0x05
+            };
+            var expected = new byte[]
+            {
+                0x36, 0x13, 0x62, 0xa8, 0x21
+            };
+
+            Permute.CryptPermute(input, true);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [Test]
+        public void Permute_Decrypts()
+        {
+            var input = new byte[]
+            {
+                0x36, 0x13, 0x62, 0xa8, 0x21
+            };
+            var expected = new byte[]
+            {
+                0x01, 0x02, 0x03, 0x04, 0x05
+            };
+
+            Permute.CryptPermute(input, false);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+    }
+}
