fe6efe9f77d4339e953b48bfe330faf4dbf05a2d
Adam McCoy <adam@forsedomani.com>
2016-01-08 07:50:15 +1100

    Add LTP heap class.

---
+++ src/Pst/Internal/Ltp/Heap.cs
@@ -0,0 +1,11 @@
+﻿namespace Pst.Internal.Ltp
+{
+    internal class Heap
+    {
+        internal byte ClientSignature { get; private set; }
+        internal uint UserRoot { get; private set; }
+        internal int AllocatedCount { get; private set; }
+        internal int FreedCount { get; private set; }
+        internal uint[] Allocations { get; private set; }
+    }
+}
