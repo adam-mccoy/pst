6d7f0e50b0930fd5bdfef32b6cf5dd50d386e518
Adam McCoy <adam@forsedomani.com>
2016-01-09 11:10:30 +1100

    Add LTP B-Tree class.

---
+++ src/Pst/Internal/Ltp/BTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pst.Internal.Ltp
+{
+    internal class BTree
+    {
+        private readonly Heap _heap;
+
+        public BTree(Heap heap)
+        {
+            _heap = heap;
+        }
+    }
+}
