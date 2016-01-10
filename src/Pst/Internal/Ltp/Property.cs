2a177d37bb2a8f6398dbe631d139e433c997e3c3
Adam McCoy <adam@forsedomani.com>
2016-01-11 08:00:33 +1100

    Add LTP Property Context class.

---
+++ src/Pst/Internal/Ltp/Property.cs
@@ -0,0 +1,9 @@
+﻿namespace Pst.Internal.Ltp
+{
+    internal class Property
+    {
+        internal ushort Key { get; set; }
+        internal ushort Type { get; set; }
+        internal uint Hnid { get; set; }
+    }
+}
---
+++ src/Pst/Internal/Ltp/PropertyContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pst.Internal.Ndb;
+
+namespace Pst.Internal.Ltp
+{
+    internal class PropertyContext
+    {
+        private Heap _heap;
+        private BTree<Property, ushort> _bTree;
+        private PstReader _pstReader;
+
+        internal PropertyContext(
+            Block block,
+            PstReader reader)
+        {
+            _heap = new Heap(block);
+            _pstReader = reader;
+            _bTree = new BTree<Property, ushort>(
+                _heap,
+                b => BitConverter.ToUInt16(b.ToArray(), 0),
+                CreateProperty);
+        }
+
+        internal IList<byte> Get(ushort key)
+        {
+            var prop = _bTree.Find(key);
+            if (prop == null)
+                return null;
+
+            if ((prop.Hnid & 0x1f) == 0)
+                return _heap[prop.Hnid];
+
+            return _pstReader.ReadBlock(prop.Hnid).Data;
+        }
+
+        private Property CreateProperty(IList<byte> bytes)
+        {
+            var buffer = bytes.ToArray();
+            return new Property
+            {
+                Key = BitConverter.ToUInt16(buffer, 0),
+                Type = BitConverter.ToUInt16(buffer, 2),
+                Hnid = BitConverter.ToUInt32(buffer, 4)
+            };
+        }
+    }
+}
