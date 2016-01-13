111edc4dc6d491f63eb4cf8820683312d9726764
Adam McCoy <adam@forsedomani.com>
2016-01-13 18:59:44 +1100

    Implement Message Store.

--- src/Pst/MessageStore.cs
+++ src/Pst/MessageStore.cs
@@ -1,16 +1,33 @@
 ﻿using Pst.Internal;
+using Pst.Internal.Ltp;
+using System.Linq;
+using System.Text;
 
 namespace Pst
 {
     public class MessageStore
     {
+        private readonly PropertyContext _context;
+
         internal MessageStore(PstReader reader)
         {
-
+            var block = reader.ReadBlock(0x21);
+            _context = new PropertyContext(block, reader);
         }
 
-        public byte[] RecordKey { get; set; }
-        public string DisplayName { get; set; }
+        public byte[] RecordKey
+        {
+            get { return _context.Get(PropertyKey.RecordKey).ToArray(); }
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                var bytes = _context.Get(PropertyKey.DisplayName).ToArray();
+                return Encoding.Unicode.GetString(bytes);
+            }
+        }
 
         public Folder RootFolder { get; set; }
         public Folder SearchFolder { get; set; }
---
+++ test/Pst.Tests/MessageStoreTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Pst.Internal;
+
+namespace Pst.Tests
+{
+    [TestFixture]
+    public class MessageStoreTests
+    {
+        [Test]
+        public void Gets_Display_Name()
+        {
+            var stream = TestHelper.GetTestDataStream("test.pst");
+            var reader = new PstReader(stream);
+
+            var messageStore = new MessageStore(reader);
+
+            Assert.AreEqual("Outlook Data File", messageStore.DisplayName);
+        }
+
+        [Test]
+        public void Gets_Record_Key()
+        {
+            var expectedBytes = new byte[]
+            {
+                0x97, 0x7b, 0x45, 0xcb, 0xdb, 0xc2, 0x82, 0x43,
+                0x92, 0x10, 0x55, 0x19, 0xe9, 0x93, 0x28, 0xfa
+            };
+            var stream = TestHelper.GetTestDataStream("test.pst");
+            var reader = new PstReader(stream);
+
+            var messageStore = new MessageStore(reader);
+
+            CollectionAssert.AreEqual(expectedBytes, messageStore.RecordKey);
+        }
+    }
+}
