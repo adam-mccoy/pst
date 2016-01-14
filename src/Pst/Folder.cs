7af1937f1a27cb70b9f5529ee5f764ca014d72f5
Adam McCoy <adam@forsedomani.com>
2016-01-14 21:04:37 +1100

    Add folder implementation.

--- src/Pst/Folder.cs
+++ src/Pst/Folder.cs
@@ -1,21 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pst.Internal;
+using Pst.Internal.Ltp;
+using Pst.Internal.Ndb;
 
 namespace Pst
 {
     public class Folder
     {
-        public long Id { get; set; }
-        public int Type { get; set; }
-        public string Name { get; set; }
-        public string Comment { get; set; }
-        public int ItemCount { get; set; }
-        public int UnreadCount { get; set; }
-        public bool HasSubfolders { get; set; }
-        public int TotalSize { get; set; }
-        public long TotalSizeExtended { get; set; }
-        public DateTime LastModified { get; set; }
+        private readonly Nid _nid;
+        private readonly IPstReader _pstReader;
+
+        private PropertyContext _properties;
+
+        internal Folder(Nid nid, IPstReader reader)
+        {
+            _nid = nid;
+            _pstReader = reader;
+            Initialize();
+        }
+
+        public string Name
+        {
+            get { return Encoding.Unicode.GetString(_properties.Get(PropertyKey.DisplayName).ToArray()); }
+        }
+
+        public int ItemCount
+        {
+            get { return BitConverter.ToInt32(_properties.Get(PropertyKey.ContentCount).ToArray(), 0); }
+        }
+
+        public int UnreadCount
+        {
+            get { return BitConverter.ToInt32(_properties.Get(PropertyKey.UnreadCount).ToArray(), 0); }
+        }
+
+        public bool HasSubfolders
+        {
+            get { return BitConverter.ToBoolean(_properties.Get(PropertyKey.Subfolders).ToArray(), 0); }
+        }
 
         public ICollection<Folder> Folders { get; set; }
+
+        private void Initialize()
+        {
+            var node = _pstReader.FindNode(_nid);
+            _properties = new PropertyContext(node, _pstReader);
+
+            var htNode = _pstReader.FindNode(Nid.ChangeType(_nid, NidType.HierarchyTable));
+        }
     }
 }
