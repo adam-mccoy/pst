776b0326374b580224c4bd32e78be96d5d87e0d6
Adam McCoy <adam@forsedomani.com>
2016-02-19 07:57:03 +1100

    Create PST editor project.

---
+++ src/PstEditor/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace PstEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog().GetValueOrDefault())
+                OpenFile(dialog.FileName);
+        }
+
+        private void OpenFile(string path)
+        {
+        }
+    }
+}
