7af59872e769641bc8c126691fe524bf6607f856
Adam McCoy <adam@forsedomani.com>
2018-11-04 22:09:28 +1100

    Add tests for multi-level BTrees.

--- test/Pst.Tests/TestHelper.cs
+++ test/Pst.Tests/TestHelper.cs
@@ -5,6 +5,15 @@ namespace Pst.Tests
 {
     public static class TestHelper
     {
-        public static Stream GetTestDataStream(string fileName) => Assembly.GetExecutingAssembly().GetManifestResourceStream("Pst.Tests.TestData." + fileName);
+        public static Stream GetTestDataStream(string fileName) =>
+            Assembly.GetExecutingAssembly().GetManifestResourceStream("Pst.Tests.TestData." + fileName);
+
+        public static byte[] GetTestDataBytes(string fileName)
+        {
+            using (var stream = GetTestDataStream(fileName))
+            {
+                return new BinaryReader(stream).ReadBytes((int)stream.Length);
+            }
+        }
     }
 }
