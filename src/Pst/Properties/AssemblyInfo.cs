c6840d0d24756d9275db75a662182c20e98665e7
Adam McCoy <adam@forsedomani.com>
2019-06-25 10:30:08 +1000

    Add PST debugger tool project

--- src/Pst/Properties/AssemblyInfo.cs
+++ src/Pst/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Pst.Tests")]
+[assembly: InternalsVisibleTo("PstDebugger")]
 [assembly: InternalsVisibleTo("BlockCrypt")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
